e665c00c2945db5aa16a939ea195074a5823a2e1
Mike Bannister <notimpossiblemike@gmail.com>
2014-04-17 22:32:49 -0700

    Oops, had to trash history

---
+++ install.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+set -e
+
+# include hidden files when globbing
+shopt -s dotglob
+
+# we need directories
+
+echo " - linking dot files into \$HOME"
+
+for file in home/*
+do
+  file_name=`basename $file`
+  echo "   * link $file to \$HOME/.$file_name"
+  rm -rf $HOME/.$file_name
+  ln -sf $PWD/$file $HOME/.$file_name
+done
+
+echo " - linking ssh files into \$HOME/.ssh"
+
+mkdir -p $HOME/.ssh
+chmod 700 $HOME/.ssh
+
+for file in ssh/*
+do
+  file_name=`basename $file`
+  echo "   * link $file to \$HOME/.ssh/$file_name"
+  rm -rf $HOME/.ssh/$file_name
+  ln -sf $PWD/$file $HOME/.ssh/$file_name
+done
+
+echo " - linking binaries into \$HOME/local/bin"
+
+mkdir -p $HOME/local/bin
+
+for file in bin/*
+do
+  file_name=`basename $file`
+  echo "   * link $file to \$HOME/local/bin/$file_name"
+  rm -rf $HOME/local/bin/$file_name
+  ln -sf $PWD/$file $HOME/local/bin/$file_name
+done
+
+echo " - deal with submodules"
+git submodule update --init
+
+echo done
---
+++ submodules.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+git submodule add https://github.com/mattn/gist-vim.git home/vim/bundle/gist
+git submodule add https://github.com/mattn/webapi-vim.git home/vim/bundle/webapi
+git submodule add https://github.com/vim-ruby/vim-ruby.git home/vim/bundle/ruby
+git submodule add https://github.com/tpope/vim-markdown.git home/vim/bundle/markdown
+git submodule add https://github.com/pangloss/vim-javascript.git home/vim/bundle/javascript
+git submodule add https://github.com/kchmck/vim-coffee-script.git home/vim/bundle/coffee
+git submodule add https://github.com/tomtom/tcomment_vim.git home/vim/bundle/tcomment
+git submodule add https://github.com/klen/python-mode.git home/vim/bundle/python
+git submodule add https://github.com/scrooloose/nerdtree.git home/vim/bundle/nerdtree
+git submodule add https://github.com/tpope/vim-surround.git home/vim/bundle/surround
+git submodule add https://github.com/tpope/vim-fugitive.git home/vim/bundle/fugitive
+git submodule add https://github.com/tpope/vim-repeat.git home/vim/bundle/repeat
+git submodule add https://github.com/tpope/vim-unimpaired.git home/vim/bundle/unimpaired
+git submodule add https://github.com/scrooloose/syntastic.git home/vim/bundle/syntastic
+git submodule add https://github.com/Lokaltog/vim-easymotion.git home/vim/bundle/easymotion
+git submodule add https://github.com/kana/vim-textobj-entire.git home/vim/bundle/entire
+git submodule add https://github.com/kana/vim-textobj-user.git home/vim/bundle/textobj-user
+git submodule add https://github.com/kana/vim-textobj-entire.git home/vim/bundle/textobj-entire
+git submodule add https://github.com/kien/ctrlp.vim.git home/vim/bundle/ctrlp
+git submodule add https://github.com/jszakmeister/vim-togglecursor.git home/vim/bundle/togglecursor

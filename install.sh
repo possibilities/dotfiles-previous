1b987c7dcd835defe20af704c6f0c8d73e72027b
Mike Bannister <notimpossiblemike@gmail.com>
2020-07-04 12:18:33 -0400

    Cleanup install script

--- install.sh
+++ install.sh
@@ -70,21 +70,59 @@ sudo apt-get install --yes \
   libgbm-dev \
   wget
 
-chsh -s $(which zsh)
+if [ -v "$ZSH_NAME" ]; then
+  chsh -s $(which zsh)
+else
+  echo " - zsh already installed"
+fi
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
-source ~/.nvm/nvm.sh
-nvm install 12 --lts
+if [ -d "${HOME}/.nvm" ] 
+then
+  echo " - nvm already installed"
+else
+  echo " - installing nvm"
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+  source ~/.nvm/nvm.sh
+  nvm install 12 --lts
+fi
 
-pip3 install --quiet --user tmuxp
+if ! [ -x "$(command -v tmuxp)" ]; then
+  echo " - installing tmuxp"
+  pip3 install --quiet --user tmuxp
+else
+  echo " - tmuxp already installed"
+fi
 
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt update && sudo apt install --no-install-recommends yarn
 
-yarn global add nodemon serve
+echo " - installing apps globally from npm"
+if ! [ -x "$(command -v sloc)" ]; then
+  yarn global add sloc
+else
+  echo " - sloc already installed"
+fi
+if ! [ -x "$(command -v nodemon)" ]; then
+  yarn global add nodemon
+else
+  echo " - nodemon already installed"
+fi
+if ! [ -x "$(command -v sloc)" ]; then
+  yarn global add serve
+else
+  echo " - serve already installed"
+fi
 
-# we need directories
+IS_HYPERFINE_INSTALLED="$(command -v hyperfine)"
+if ! [ -x ${IS_HYPERFINE_INSTALLED} ]; then
+  wget \
+    https://github.com/sharkdp/hyperfine/releases/download/v1.10.0/hyperfine_1.10.0_amd64.deb \
+    -O /tmp/hyperfine.deb
+  sudo dpkg -i /tmp/hyperfine.deb
+else
+  echo " - hyperfine already installed"
+fi
 
 echo " - linking dot files into \$HOME"
 
@@ -124,4 +162,10 @@ done
 echo " - deal with vim plugins"
 ./install-vim-plugins.sh || true
 
-echo done
+if ! [ -x ${IS_HYPERFINE_INSTALLED} ]; then
+  echo WARNINGS:
+  echo  " - INSTALLED HYPERFINE HARDCODED VERSION NUMBER, SEE dotfiles/install.sh"
+fi
+
+echo
+echo ALL DONE, COMPLETE, OK, NICE, WORD, HAVE FUN SUCKA

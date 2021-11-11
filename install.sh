9a6718183eb02c023609a19d9ac50e1962927bec
Mike Bannister <notimpossiblemike@gmail.com>
2021-11-11 16:25:51 -0500

    Cleanup install

--- install.sh
+++ install.sh
@@ -29,86 +29,59 @@ sudo apt-get install --yes \
   pass \
   terminator \
   gnome-tweaks \
-  x11-utils \
-  gnome-shell-extension-autohidetopbar \
   gnome-shell-extension-pixelsaver \
-  gnome-shell-extension-unite-shell
+  gnome-shell-extension-autohidetopbar
 
-# OBS and dependencies
-apt-get install --yes \
-  ffmpeg \
-  v4l2loopback-dkms \
+if [[ -v UPGRADE ]] || [ ${SHELL} == "/usr/bin/bash" ]; then
+  echo "setting zsh shell to default, enter password"
+  chsh -s $(which zsh)
+else
+  echo " - zsh shell already default"
+fi
 
-sudo add-apt-repository ppa:obsproject/obs-studio --yes
-sudo apt update
-sudo apt install obs-studio --yes
+if [[ -v UPGRADE ]] || ! [ -x "$(command -v git)" ]; then
+  echo " - installing git from ppa"
+  sudo add-apt-repository ppa:git-core/ppa --yes
+  sudo apt update
+  sudo apt install git --yes
+else
+  echo " - git already installed"
+fi
+
+if [[ -v UPGRADE ]] || ! [ -x "$(command -v obs)" ]; then
+  echo " - installing obs from ppa"
+  # OBS and dependencies
+  sudo apt-get install --yes \
+    ffmpeg \
+    v4l2loopback-dkms \
+
+  sudo add-apt-repository ppa:obsproject/obs-studio --yes
+  sudo apt update
+  sudo apt install obs-studio --yes
+else
+  echo " - obs already installed"
+fi
 
 # TODO install hub in some way, doesn't work on ubuntu vm
 
-# install neovim from ppa
-sudo add-apt-repository ppa:neovim-ppa/unstable --yes
-sudo apt-get update
-sudo apt-get install neovim --yes
+if [[ -v UPGRADE ]] || ! [ -x "$(command -v nvim)" ]; then
+  echo " - installing neovim from ppa"
+  # install neovim from ppa
+  sudo add-apt-repository ppa:neovim-ppa/unstable --yes
+  sudo apt-get update
+  sudo apt-get install neovim --yes
+else
+  echo " - neovim already installed"
+fi
 
-# install git from ppa
-sudo add-apt-repository ppa:git-core/ppa --yes
-sudo apt update
-sudo apt install git --yes
-
-# For puppeteer
-sudo apt-get install --yes \
-  gconf-service \
-  libasound2 \
-  libatk1.0-0 \
-  libatk-bridge2.0-0 \
-  libc6 \
-  libcairo2 \
-  libcups2 \
-  libdbus-1-3 \
-  libexpat1 \
-  libfontconfig1 \
-  libgcc1 \
-  libgconf-2-4 \
-  libgdk-pixbuf2.0-0 \
-  libglib2.0-0 \
-  libgtk-3-0 \
-  libnspr4 \
-  libpango-1.0-0 \
-  libpangocairo-1.0-0 \
-  libstdc++6 \
-  libx11-6 \
-  libx11-xcb1 \
-  libxcb1 \
-  libxcomposite1 \
-  libxcursor1 \
-  libxdamage1 \
-  libxext6 \
-  libxfixes3 \
-  libxi6 \
-  libxrandr2 \
-  libxrender1 \
-  libxss1 \
-  libxtst6 \
-  ca-certificates \
-  fonts-liberation \
-  libappindicator1 \
-  libnss3 \
-  lsb-release \
-  xdg-utils \
-  libgbm-dev \
-  wget
-
-mkdir -p ${HOME}/.vim/backups
-
-chsh -s $(which zsh)
-
-if ! [ -x "$(command -v lab)" ]; then
+if [[ -v UPGRADE ]] || ! [ -x "$(command -v lab)" ]; then
+  echo " - installing lab"
   curl -s https://raw.githubusercontent.com/zaquestion/lab/master/install.sh | sudo bash
 else
   echo " - lab already installed"
 fi
 
-if [ -d "${HOME}/.nvm" ] 
+if [[ -v UPGRADE ]] || [ -d "${HOME}/.nvm" ] 
 then
   echo " - nvm already installed"
 else
@@ -119,16 +92,83 @@ else
   nvm install 14 --lts
 fi
 
-if ! [ -x "$(command -v tmuxp)" ]; then
+if [[ -v UPGRADE ]] || ! [ -x "$(command -v tmuxp)" ]; then
   echo " - installing tmuxp"
   pip3 install --quiet --user tmuxp
 else
   echo " - tmuxp already installed"
 fi
 
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt update && sudo apt install --no-install-recommends yarn
+if [[ -v UPGRADE ]] || ! [ -x "$(command -v yarn)" ]; then
+  echo " - installing yarn"
+  curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+  echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+  sudo apt update && sudo apt install --no-install-recommends yarn
+else
+  echo " - yarn already installed"
+fi
+
+if [[ -v UPGRADE ]] || [ -d "${HOME}/.powerlevel10k" ]
+then
+  echo " - power level 10k already installed"
+else
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.powerlevel10k
+fi
+
+echo " - deal with vim plugins"
+
+mkdir -p ${HOME}/.vim/backups
+
+if [[ -v UPGRADE ]] || [ -f "${HOME}/.vim/autoload/plug.vim" ]
+then
+  echo " - vim plug already installed"
+  vim +'PlugInstall --sync' +qa
+else
+  curl -sfLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+
+
+# Python
+
+MINICONDA_SCRIPT_NAME="Miniconda3-latest-Linux-x86_64.sh"
+
+if [[ -v UPGRADE ]] || [ -d "${HOME}/miniconda3" ]
+then
+  echo " - miniconda already installed"
+else
+  rm -rf ${MINICONDA_SCRIPT_NAME}
+  wget "https://repo.anaconda.com/miniconda/${MINICONDA_SCRIPT_NAME}"
+
+  bash ${MINICONDA_SCRIPT_NAME} -b
+
+  rm ${MINICONDA_SCRIPT_NAME}
+fi
+
+# Cad stack
+if [[ -v UPGRADE ]] || ! [ -x "$(command -v kicad-nightly)" ]; then
+  echo " - installing kicad-nightly from ppa"
+  sudo add-apt-repository --yes ppa:kicad/kicad-5.1-releases
+  sudo apt update
+  sudo apt install --yes --install-recommends kicad
+else
+  echo " - kicad-nightly already installed"
+fi
+
+if [[ -v UPGRADE ]] || ! [ -x "$(command -v obs)" ]; then
+  echo " - install cadquery and cadquery editor"
+  conda install -c cadquery -c conda-forge cadquery=master --yes
+  conda install -c cadquery -c conda-forge cq-editor=master --yes
+else
+  echo " - cadquery and cadquery editor already installed"
+fi
+
+
+if [[ -v UPGRADE ]] || ! [ -x "$(command -v black)" ]; then
+  echo " - installing black"
+  pip3 install black
+else
+  echo " - black already installed"
+fi
 
 echo " - linking dot files into \$HOME"
 
@@ -138,6 +178,18 @@ do
   echo "   * link $file to \$HOME/.$file_name"
   rm -rf $HOME/.$file_name
   ln -sf $PWD/$file $HOME/.$file_name
+done
+
+echo " - linking config files into \$HOME\config"
+
+mkdir -p ~/.config
+
+for file in config/*
+do
+  file_name=`basename $file`
+  echo "   * link $file to \$HOME/.config/$file_name"
+  rm -rf $HOME/.config/$file_name
+  ln -sf $PWD/$file $HOME/.config/$file_name
 done
 
 echo " - linking ssh files into \$HOME/.ssh"
@@ -157,24 +209,6 @@ echo " - linking binaries into \$HOME/local/bin"
 
 mkdir -p $HOME/local/bin
 
-
-echo " - deal with vim plugins"
-
-if [ -d "${HOME}/.powerlevel10k" ]
-then
-  echo " - power level 10k already installed"
-else
-  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.powerlevel10k
-fi
-
-if [ -f "${HOME}/.vim/autoload/plug.vim" ]
-then
-  echo " - vim plug already installed"
-else
-  curl -sfLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-fi
-
-vim +'PlugInstall --sync' +qa
 
 echo
 echo ALL DONE, COMPLETE, OK, NICE, WORD, HAVE FUN SUCKA
